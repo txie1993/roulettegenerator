92235b161fa9f00a813471f94c5ce1a8fe467993
Tony Xie <tony@tonyxie.co>
2018-09-16 01:34:13 -0700

    rerolls

--- randomize_people.py
+++ randomize_people.py
@@ -3,6 +3,8 @@ import random
 
 from Person import Person
 
+MAX_TREASURER_COUNT = 6
+
 saved_data = {
   'people': [
     {
@@ -90,7 +92,7 @@ if __name__ == '__main__':
     people.append(Person(json_person['name'], json_person['history'], json_person['is_treasurer']))
 
   for person in people:
-    if person.is_treasurer:
+    if person.is_treasurer and len(treasurers) <= MAX_TREASURER_COUNT:
       treasurers.append(person)
     else:
       non_treasurers.append(person)
@@ -102,22 +104,34 @@ if __name__ == '__main__':
       'members': []
     }
 
-  random.shuffle(non_treasurers)
-  for index, person in enumerate(non_treasurers):
-    groups[index%3]['members'].append(person)
+  keep_generating_groups = True
+  while keep_generating_groups: 
+    random.shuffle(non_treasurers)
+    for index, person in enumerate(non_treasurers):
+      groups[index%3]['members'].append(person)
 
-  for group_number in groups:
-    group = groups[group_number]
-    group_members = [group['treasurer']] + group['members']
-    group_names = [member.name for member in group_members]
-    for member in group_members:
-      print member.history
-      for name in group_names:
-        if not name == member.name:
-          member.add_history(name)
-        json_member = [json_entry for json_entry in people_data['people'] if json_entry['name'] == name][0]
-        json_member['history'] = member.history
-      print member.history
+    group_member_names = []
+    for group_number in groups:
+      group = groups[group_number]
+      group_members = [group['treasurer']] + group['members']
+      group_names = [member.name for member in group_members]
+      group_member_names.append(group_names)
+      for member in group_members:
+        for name in group_names:
+          if not name == member.name:
+            member.add_history(name)
+          json_member = [json_entry for json_entry in people_data['people'] if json_entry['name'] == name][0]
+          json_member['history'] = member.history
+    
+    print group_member_names
+    should_continue = raw_input('Keep this configuration? (y/N)')
+    if should_continue == 'y':
+      keep_generating_groups = False
+    else:
+      for group_number in groups:
+        group = groups[group_number]
+        group['members'] = []
 
-    with open('data.json', 'w') as outfile:
-      json.dump(people_data, outfile)
+
+  with open('data.json', 'w') as outfile:
+    json.dump(people_data, outfile)
